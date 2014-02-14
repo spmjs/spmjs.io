ad8f5367bc82f67c0f8e2cd3326c6a412eb0dd26
afc163 <afc163@gmail.com>
2014-02-14 16:58:32 +0800

    abortify

--- routes/repository.js
+++ routes/repository.js
@@ -11,7 +11,7 @@ exports.project = {
     res.send("respond with a resource");
   },
   delete: function(req, res) {
-    res.send({
+    res.send(200, {
       status: 'info',
       message: 'Project is deleted.'
     });
@@ -32,7 +32,8 @@ exports.package = {
     var data = req.body;
     var package = new Package(data);
     if (!package) {
-      res.send(404, {
+      abortify(res, {
+        code: 404,
         message: 'Package not found.'
       });
       return;
@@ -40,7 +41,7 @@ exports.package = {
     uploadPackage(package, req, res);
   },
   delete: function(req, res) {
-    res.send({
+    res.send(200, {
       status: 'info',
       message: 'Project is deleted.'
     });
@@ -54,7 +55,8 @@ function uploadPackage(package, req, res) {
     ctype = 'application/x-tar-gz'
   }
   if (ctype !== 'application/x-tar-gz' && ctype !== 'application/x-tgz') {
-    res.send(415, {
+    abortify(res, {
+      code: 415,
       message: 'Only gziped tar file is allowed.'
     });
   }
@@ -62,12 +64,34 @@ function uploadPackage(package, req, res) {
   package.md5 = crypto.createHash('md5').update(req.body).digest('hex');
   var md5 = req.headers['x-package-md5'];
   if (md5 && md5 !== package.md5) {
-    res.send(400, {
+    abortify(res, {
+      code: 400,
       message: 'MD5 does not match.'
     });
   }
 
   res.send(200);
+}
+
+function abortify(res, options) {
+  code = options.code || 401;
+  status = options.status || 'error';
+
+  var msgs = {
+    400: 'Bad request.',
+    401: 'Authorization required.',
+    403: 'Permission denied.',
+    404: 'Not found.',
+    406: 'Not acceptable.',
+    415: 'Unsupported media type.',
+    426: 'Upgrade required.',
+    444: 'Force option required.'
+  };
+  message = options.message || msgs[code];
+  res.send(code, {
+    status: status,
+    message: message
+  });
 }
 
 // TODO:
